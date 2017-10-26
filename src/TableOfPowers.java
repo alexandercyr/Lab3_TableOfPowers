0029119f8feae8479064bd44ef2b837340ce9725
Alex Cyr <alexcyr@Alex-Cyrs-MacBook-Pro.local>
2017-10-25 20:44:34 -0400

    update comments, edit input validation

--- src/TableOfPowers.java
+++ src/TableOfPowers.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 import java.lang.Math;
 
+/**
+ *	Program requests user to input a number and outputs
+ *	a table of squared and cubed numbers from 1 up to 
+ *	the number provided by the user.
+ *
+ *	@author Alexander Cyr
+ *
+ */
+
 public class TableOfPowers {
 	
 	static Scanner scnr = new Scanner(System.in);
@@ -12,6 +21,7 @@ public class TableOfPowers {
 		
 		System.out.println("Learn your squares and cubes!");
 		
+		//Loop the program until ended by user
 		while(continueRun.equalsIgnoreCase("y")) {
 			
 			System.out.println();
@@ -23,6 +33,7 @@ public class TableOfPowers {
 			System.out.println("Number      Squared     Cubed        ");
 			System.out.println("======      =======     =====        ");
 			
+			//for each number up to user number, print formatted number, squared and cubed
 			for(int i = 1; i <= userVal; ++i) {
 				System.out.format("%-12d%-12d%-12d", i, Math.round(Math.pow(i, 2)), Math.round(Math.pow(i, 3)));
 				System.out.println();
@@ -44,7 +55,8 @@ public class TableOfPowers {
 		boolean valid = false;
 		
 		while (!valid) {
-
+			
+			//Loop until user provides a number
 			while (!scnr.hasNextInt()) {
 				System.out.print("Invalid entry. Enter a number greater than 0: ");
 				scnr.nextLine();
@@ -52,7 +64,8 @@ public class TableOfPowers {
 
 			userNumber = scnr.nextInt();
 
-			if (userNumber > 0 && userNumber <= 100) {
+			//Check to see if user number is greater than zero
+			if (userNumber > 0) {
 				valid = true;
 			} else {
 				System.out.print("Invalid entry. Enter a number greater than 0: ");
